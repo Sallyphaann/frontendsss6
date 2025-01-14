d882584924617ec659b929edda0535f866f9adcf
Sallyphaann <92030592+Sallyphaann@users.noreply.github.com>
2025-01-14 15:18:16 +0100

    change file

---
+++ cypress/cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://frontend:3000', // Replace with your frontend URL
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}', // Ensure this matches your test files
+    supportFile: false,
+  },
+});
